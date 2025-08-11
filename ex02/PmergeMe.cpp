19a0f8c5b8164621dc6356e6ccf1ebd6a82c26a0
MemeSamu14 <samuele.fiorini13@gmail.com>
2025-08-11 10:41:44 +0200

    correction to ex02, i need to change the stl contaainer vector in the first exercise

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: sfiorini <sfiorini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/10 10:57:30 by sfiorini          #+#    #+#             */
-/*   Updated: 2025/08/10 12:22:16 by sfiorini         ###   ########.fr       */
+/*   Updated: 2025/08/11 10:41:26 by sfiorini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,14 +34,34 @@ const char *PmergeMe::ErrorProgram::what() const throw()
 
 void	PmergeMe::setValues(const std::string &str)
 {
+	int j = 0;
 	for (int i = 0; i < static_cast<int>(str.size()); i++)
 	{
 		if (str[i] != ' ')
 		{
+			j = 0;
+			while (str[i + j] >= '0' && str[i + j] <= '9')
+				j++;
 			if (str[i] >= '0' && str[i] <= '9')
 			{
-				this->dq.push_back(str[i] - 48);
-				this->lis.push_back(str[i] - 48);
+				if (j == 1)
+				{
+					this->dq.push_back(str[i] - 48);
+					this->vet.push_back(str[i] - 48);
+				}
+				else
+				{
+					std::cout << "str[i]: " << str[i] << std::endl;
+					std::string	num;
+					int z;
+					for (z = 0; z < j; z++)
+					{
+						num.push_back(str[i + z]);
+					}
+					this->dq.push_back(std::atoi(num.c_str()));
+					this->vet.push_back(std::atoi(num.c_str()));
+					i += z;
+				}
 			}
 			else
 				throw	PmergeMe::ErrorProgram();
@@ -53,11 +73,6 @@ void	PmergeMe::sort(const std::string &str)
 {
 	std::cout << "Before: " << str << std::endl;
 	
-	// int	dqTimeStart = 0;
-	// int	lisTimeStart = 0;
-	// int	dqTimeEnd = 0;
-	// int	lisTimeEnd = 0;
-	
 	int	left = 0;
 	int	right = this->dq.size() - 1;
 
@@ -68,89 +83,81 @@ void	PmergeMe::sort(const std::string &str)
 	gettimeofday(&dqTimeEnd, NULL);
 	double dqTime = static_cast<double>((dqTimeEnd.tv_sec - dqTimeStart.tv_sec) * 1000000LL + (dqTimeEnd.tv_usec - dqTimeStart.tv_usec)) / 1000000.0;
 
-	struct timeval lisTimeStart;
-	struct timeval lisTimeEnd;
-	gettimeofday(&lisTimeStart, NULL);
-	// lisMergeSort(left, right);
-	this->lis.sort();
-	gettimeofday(&lisTimeEnd, NULL);
-	double lisTime = static_cast<double>((lisTimeEnd.tv_sec - lisTimeStart.tv_sec) * 1000000LL + (lisTimeEnd.tv_usec - lisTimeStart.tv_usec)) / 1000000.0;
+	struct timeval vetTimeStart;
+	struct timeval vetTimeEnd;
+	gettimeofday(&vetTimeStart, NULL);
+	vetMergeSort(left, right);
+	gettimeofday(&vetTimeEnd, NULL);
+	double vetTime = static_cast<double>((vetTimeEnd.tv_sec - vetTimeStart.tv_sec) * 1000000LL + (vetTimeEnd.tv_usec - vetTimeStart.tv_usec)) / 1000000.0;
+
+	std::cout << "After:  ";
+	for (int i = 0; i < static_cast<int>(this->vet.size()); i++)
+		std::cout << this->vet[i] << " ";
+	std::cout << std::endl;
 
 	std::cout << "Tempo trascorso in millisecondi: " << std::fixed << std::setprecision(10) << dqTime << std::endl;
-	std::cout << "Tempo trascorso in millisecondi: " << std::fixed << std::setprecision(10) << lisTime << std::endl;
+	std::cout << "Tempo trascorso in millisecondi: " << std::fixed << std::setprecision(10) << vetTime << std::endl;
 	
-	std::cout << "After:  ";
+
 	
-	for (int i = 0; i < static_cast<int>(dq.size()); i++)
-		std::cout << dq[i] << " ";
-	std::cout << std::endl;
-	
-	for (std::list<int>::iterator it = this->lis.begin(); it != this->lis.end(); ++it)
-	{
-		std::cout << *it << " ";
-	}
-	// lisMergeSort(left, right);
-	
-	// std::cout << "Time to process a range of " << this->dq.size() << " with std::dque : " << dqTime;
-	// std::cout << "Time to process a range of " << this->dq.size() << " with std::list : " << lisTime;
 }
 
-void	PmergeMe::lisMergeSort(int left, int right)
+void	PmergeMe::vetMergeSort(int left, int right)
 {
 	if (left >= right)
 		return;
 
 	int mid = left + (right - left) / 2;
-	dqMergeSort(left, mid);
-	dqMergeSort(mid + 1, right);
-	dqMerge(left, mid, right);
+	vetMergeSort(left, mid);
+	vetMergeSort(mid + 1, right);
+	vetMerge(left, mid, right);
 }
 
-// void	PmergeMe::lisMerge(int left, int mid, int right)
-// {
-// 	int n1 = mid - left + 1;
-// 	int n2 = right - mid;
+void	PmergeMe::vetMerge(int left, int mid, int right)
+{
+	int n1 = mid - left + 1;
+	int n2 = right - mid;
 
-// 	std::list<int> L(n1), R(n2);
+	std::vector<int> L(n1), R(n2);
 
-// 	for (int i = 0; i < n1; i++)
-// 		L[i] = this->lis[left + i];
-// 	for (int j = 0; j < n2; j++)
-// 		R[j] = this->lis[mid + 1 + j];
+	for (int i = 0; i < n1; i++)
+		L[i] = this->vet[left + i];
+	for (int j = 0; j < n2; j++)
+		R[j] = this->vet[mid + 1 + j];
 
-// 	int i = 0, j = 0;
-// 	int k = left;
+	int i = 0, j = 0;
+	int k = left;
 
-// 	while (i < n1 && j < n2) 
-// 	{
-// 		if (L[i] <= R[j]) 
-// 		{
-// 			this->lis[k] = L[i];
-// 			i++;
-// 		}
-// 		else
-// 		{
-// 			this->lis[k] = R[j];
-// 			j++;
-// 		}
-// 		k++;
-// 	}
+	while (i < n1 && j < n2) 
+	{
+		if (L[i] <= R[j]) 
+		{
+			this->vet[k] = L[i];
+			i++;
+		}
+		else
+		{
+			this->vet[k] = R[j];
+			j++;
+		}
+		k++;
+	}
 	
-// 	while (i < n1)
-// 	{
-// 		this->lis[k] = L[i];
-// 		i++;
-// 		k++;
-// 	}
+	while (i < n1)
+	{
+		this->vet[k] = L[i];
+		i++;
+		k++;
+	}
 
-// 	while (j < n2)
-// 	{
-// 		this->lis[k] = R[j];
-// 		j++;
-// 		k++;
-// 	}
+	while (j < n2)
+	{
+		this->vet[k] = R[j];
+		j++;
+		k++;
+	}
 	
-// }
+}
 
 
 
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,14 +6,14 @@
 /*   By: sfiorini <sfiorini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/10 10:57:30 by sfiorini          #+#    #+#             */
-/*   Updated: 2025/08/10 12:19:23 by sfiorini         ###   ########.fr       */
+/*   Updated: 2025/08/11 10:24:35 by sfiorini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PMERGEME_HPP
 # define PMERGEME_HPP
 
-#include <list>
+#include <vector>
 #include <iostream>
 #include <string>
 #include <cstdlib>
@@ -24,12 +24,12 @@
 class PmergeMe
 {
 	private:
-		std::deque<int>	dq;
-		std::list<int>	lis;
+		std::deque<int>		dq;
+		std::vector<int>	vet;
 		void	dqMerge(int left, int mid, int right);
 		void	dqMergeSort(int left, int right);
-		void	lisMerge(int left, int mid, int right);
-		void	lisMergeSort(int left, int right);
+		void	vetMerge(int left, int mid, int right);
+		void	vetMergeSort(int left, int right);
 	public:
 		class ErrorProgram: public std::exception
 		{
